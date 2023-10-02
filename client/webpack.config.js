e363e080e480db412a3da9f96ba44d7d178b77c7
Camille MacLaren <camillemaclaren@gmail.com>
2023-10-01 20:06:00 -0500

    Update webpack.config.js
    add plugins for html webpack,  inject manifest (for service worker), and webpackmanifest for manifest.json
    add loader for css, images, and babel loader for es5 conversion

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,11 +15,62 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // TODO: Add and configure workbox plugins for a service worker and manifest file.
+      
+      // simplifies creation of HTML files to serve your webpack bundles.
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+      // service worker stuff
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      // manifest json
+      new WebpackPwaManifest({
+        name: 'Text Editor',
+        short_name: 'Txt Ed',
+        description: 'Edit text',
+        background_color: '#7eb4e2', //lightish blue
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
       
     ],
 
     module: {
+      // TODO: Add CSS loaders and babel to webpack.
       rules: [
+        {
+          // css loader (looks for any file that ends with .css)
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          //used for image files
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          //babel loader helps converts javascript from ES6 to ES5
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
         
       ],
     },
