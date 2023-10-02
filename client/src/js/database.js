34a27b395e147d22bd02bb0043b11bf46350540a
Camille MacLaren <camillemaclaren@gmail.com>
2023-10-01 22:49:03 -0500

    Put method for indexdb in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  // connect to database
+  const contentDb = await openDB('jate', 1);
+  // create transaction
+  const tx = contentDb.transaction('jate', 'readwrite');
+  // open desired object store
+  const store = tx.objectStore('jate');
+  // request to edit info with put method
+  const request = store.put({id: id, value: content})
+  // confirmation of req
+  const result = await request;
+  // tell console was success
+  console.log('Data saved to the database', result);
+  //if error
+  console.error('putDb not implemented');
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
